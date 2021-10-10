9639000435cfee15795dc6b0d6ce733d741ffaef
trofidal <trofidal@trofidals-iMac.local>
2021-10-10 11:21:33 -0700

    fix substr first

---
+++ srcs/substr.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   substr.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trofidal <trofidal@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 10:25:32 by trofidal          #+#    #+#             */
+/*   Updated: 2021/10/10 11:21:19 by trofidal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+extern "C" {
+#define new bar
+#include "../../libft.h"
+#undef new
+};
+
+#include "../includes/main_header.hpp"
+
+std::string testing;
+int         test_number;
+
+int main(void){
+    std::string fN = "substr"; //Function Name
+    Infos   trofidal(fN, 6);
+    signal(SIGSEGV, segfault_handler);
+    
+    #define a ft_substr
+    #define b strndup
+    #define s std::to_string
+    #define t setTesting
+    
+    char    *realSubstr;
+    char    *fakeSubstr;
+    int     ret = 0, retReal = 0;
+
+    fakeSubstr = a("", 0, 100);realSubstr = b("", 100);
+    /*1*/trofidal.t(fN, "");trofidal.tIntPtr(strcmp(fakeSubstr, realSubstr) == 0 ? 0 : 1, strlen(fakeSubstr) == strlen(realSubstr));
+    trofidal.tripouilleCheck(fakeSubstr, 1);free(fakeSubstr); free(realSubstr);trofidal.showLeaks( ret, retReal );
+
+    fakeSubstr = a("42Nice", 2, 50);realSubstr = b("42Nice" + 2, 50);
+    /*2*/trofidal.t(fN, "42Nice, 2, 50");trofidal.tIntPtr(strcmp(fakeSubstr, realSubstr) == 0 ? 0 : 1, strlen(fakeSubstr) == strlen(realSubstr));
+    trofidal.tripouilleCheck(fakeSubstr, 5);free(fakeSubstr); free(realSubstr);trofidal.showLeaks( ret, retReal );
+    
+    fakeSubstr = a("hello everyone !", 50, 20);realSubstr = b("hello everyone !", 0);
+    /*3*/trofidal.t(fN, "hello everyone !, 50, 20");trofidal.tIntPtr(strcmp(fakeSubstr, realSubstr) == 0 ? 0 : 1, strlen(fakeSubstr) == strlen(realSubstr));
+    trofidal.tripouilleCheck(fakeSubstr, 1);free(fakeSubstr); free(realSubstr);trofidal.showLeaks( ret, retReal );
+    
+    fakeSubstr = a("aaaaaaaaaaaaaaaaaaaaaaaaa", 0, 200);realSubstr = b("aaaaaaaaaaaaaaaaaaaaaaaaa", 200);
+    /*4*/trofidal.t(fN, "aaaaaaaaaaaaaaaaaaaaaaaaa");trofidal.tIntPtr(strcmp(fakeSubstr, realSubstr) == 0 ? 0 : 1, strlen(fakeSubstr) == strlen(realSubstr));
+    trofidal.tripouilleCheck(fakeSubstr, 26);free(fakeSubstr); free(realSubstr);trofidal.showLeaks( ret, retReal );
+
+    fakeSubstr = a("0560ez65r1rtye961rty650er56tyh0r65", 5, 20);realSubstr = b("0560ez65r1rtye961rty650er56tyh0r65f" + 5, 20);
+    /*5*/trofidal.t(fN, "0560ez65r1rtye961rty650er56tyh0r65, 5, 20");trofidal.tIntPtr(strcmp(fakeSubstr, realSubstr) == 0 ? 0 : 1, strlen(fakeSubstr) == strlen(realSubstr));
+    trofidal.tripouilleCheck(fakeSubstr, 21);free(fakeSubstr); free(realSubstr);trofidal.showLeaks( ret, retReal );
+    ataoiiiii, FIX ME THOMAS
+    fakeSubstr = a("pendant (str[i])", 16, 17);realSubstr = b("pendant (str[i])" + 16, 17);
+    /*6*/trofidal.t(fN, "pendant (str[i]), 16, 17");trofidal.tIntPtr(strcmp(fakeSubstr, realSubstr) == 0 ? 0 : 1, strlen(fakeSubstr) == strlen(realSubstr)); 
+    trofidal.tripouilleCheck(fakeSubstr, 2);free(fakeSubstr); free(realSubstr);trofidal.showLeaks( ret, retReal );
+}
